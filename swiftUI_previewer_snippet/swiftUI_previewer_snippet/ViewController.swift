bfb82800204e7e2dfc1d15532fbdd2d1a1e1a91f
Nikita Shinov <shinov_96@mail.ru>
2022-07-18 19:30:28 +0300

    Previewer file added

---
+++ swiftUI_previewer_snippet/swiftUI_previewer_snippet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftUI_previewer_snippet
+//
+//  Created by max on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
